87814de96bb9d62704321d5026368b2e07414652
Denner Fernandes <denners777@hotmail.com>
2018-10-24 12:04:31 -0300

    Init layout
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

---
+++ index.php
@@ -0,0 +1,134 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <!-- Required meta tags always come first -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Zarema Reforma de Máquinas</title>
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+        <!-- Bootstrap core CSS -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Material Design Bootstrap -->
+        <link href="/assets/css/mdb.min.css" rel="stylesheet">
+        <style>
+            /* Required for full background image */
+
+            html,
+            body,
+            header,
+            .view {
+                height: 100%;
+            }
+
+            @media (max-width: 740px) {
+                html,
+                body,
+                header,
+                .view {
+                    height: 100vh;
+                }
+            }
+
+            .top-nav-collapse {
+                background-color: #f8bbd0 !important;
+            }
+
+            .navbar:not(.top-nav-collapse) {
+                background: transparent !important;
+            }
+
+            @media (max-width: 991px) {
+                .navbar:not(.top-nav-collapse) {
+                    background: #f8bbd0 !important;
+                }
+            }
+        </style>
+    </head>
+
+    <body>
+        <!-- Main navigation -->
+        <header>
+            <!-- Navbar -->
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top scrolling-navbar">
+                <div class="container">
+                    <a class="navbar-brand" href="#"><strong>Zarema</strong></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                        </ul>
+                        <form class="form-inline">
+                            <div class="md-form my-0">
+                                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+            <!-- Navbar -->
+            <!-- Full Page Intro -->
+            <div class="view  jarallax" data-jarallax='{"speed": 0.2}' style="background-image: url('assets/img/bg.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+                <!-- Mask & flexbox options-->
+                <div class="mask rgba-purple-slight d-flex justify-content-center align-items-center">
+                    <!-- Content -->
+                    <div class="container">
+                        <!--Grid row-->
+                        <div class="row wow fadeIn">
+                            <!--Grid column-->
+                            <div class="col-md-12 text-center">
+                                <h1 class="display-4 font-weight-bold mb-0 pt-md-5 pt-5 wow fadeInUp">Zarema</h1>
+                                <h5 class="pt-md-5 pt-sm-2 pt-5 pb-md-5 pb-sm-3 pb-5 wow fadeInUp" data-wow-delay="0.2s">Reforma de Máquinas de Panificação</h5>
+                                <div class="wow fadeInUp" data-wow-delay="0.4s">
+                                    <address><strong>Rua Ari Parreiras Barbosa, 651 - Planalto - Divinópolis, MG - CEP: 35500-001</strong></address>
+                                    <p class="phone"><strong>(37) 3216-0569</strong></p>
+                                </div>
+                            </div>
+                            <!--Grid column-->
+                        </div>
+                        <!--Grid row-->
+                    </div>
+                    <!-- Content -->
+                </div>
+                <!-- Mask & flexbox options-->
+            </div>
+            <!-- Full Page Intro -->
+        </header>
+        <!-- Main navigation -->
+        <!-- Main Layout -->
+        <main>
+            <div class="container">
+                <!-- Grid row -->
+                <div class="row py-5">
+                    <!-- Grid column -->
+                    <div class="col-md-12 text-center">
+                        <p></p>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
+            </div>
+        </main>
+        <!-- Main Layout -->
+        <!-- SCRIPTS -->
+        <!-- JQuery -->
+        <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+        <!-- Tooltips -->
+        <script type="text/javascript" src="assets/js/popper.min.js"></script>
+        <!-- Bootstrap core JavaScript -->
+        <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+        <!-- MDB core JavaScript -->
+        <script type="text/javascript" src="assets/js/mdb.min.js"></script>
+        <script>
+            new WOW().init();
+        </script>
+    </body>
+
+</html>
